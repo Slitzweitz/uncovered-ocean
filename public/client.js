2541aa079d2a650eceea715d53b62239bc5daaec
System <none>
2017-08-11 01:15:05 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -5,22 +5,27 @@
 // add other scripts at the bottom of index.html
 
 $(function() {
-  console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
+  console.log('hello world from client.js :o');
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    
-    var XHR = new XMLHttpRequest();
-    var FD = new FormData();
-    
-    
-  });
+  var form = document.forms.namedItem("fileinput");
+  form.addEventListener('submit', function(ev) {
+
+    var oOutput = document.querySelector("div"),
+        oData = new FormData(form);
+
+    var oReq = new XMLHttpRequest();
+    oReq.open("POST", "stash.php", true);
+    oReq.onload = function(oEvent) {
+      if (oReq.status == 200) {
+        oOutput.innerHTML = "Uploaded!";
+      } else {
+        oOutput.innerHTML = "Error " + oReq.status + " occurred when trying to upload your file.<br \/>";
+      }
+  };
+
+  oReq.send(oData);
+  ev.preventDefault();
+}, false);
 
 });
 
