176c064645bbf0e99ca8c49885cb8e9608378e48
System <none>
2017-08-13 02:02:24 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ when submit is pressed:
 // init project
 var express = require('express');
 var multer = require('multer');
-var upload = multer();
+var upload = multer({
+  dest: "./",
+  limits: {
+    files: 1
+  },
+  inMemory: true
+});
 var app = express();
 
 // we've started you off with Express, 
@@ -26,14 +32,6 @@ var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-app.use(multer({
-  dest: "./",
-  limits: {
-    files: 1
-  },
-  inMemory: true
-}))
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -46,19 +44,12 @@ app.get("/dreams", function (request, response) {
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
 app.post("/", upload.single('fileSize'), function (request, response) {
-  let form = request.file;
+  let fileSize = request.file.size;
     if (request.file) {
-      console.log('theres a file in there: ', form);
+      console.log('theres a file in there: ', fileSize);
     }
   response.sendStatus(200);
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
